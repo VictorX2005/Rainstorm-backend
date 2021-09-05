44f4b1966b0790b572f6d7f5dcfd08db20542d6c
Derek Hsieh <devderekhsieh@gmail.com>
2021-09-04 23:11:24 -0700

    connect to mongodb with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 app = express();
 
-require('dotenv').config();
+// mongodb configuration -> database uri is located in .env file
+const uri = process.env.URI;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongodb connection
+mongoose.connect(uri);
+
+mongoose.connection.on("connected", ()=> {
+    console.log("Connected to MongoDB using MongooseJS");
+});
+
+
 app.set('view engine', 'ejs');
 app.use("/", require("./routes/hello"))
 
